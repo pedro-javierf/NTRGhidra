180a7ce3b9417efcdebdeaea8e69d1b9f2602ff3
Pedro Javier <pedro-javierf@users.noreply.github.com>
2020-03-25 12:24:39 +0100

    Add files via upload

---
+++ build/help/main/help/NTRGhidra_HelpSet.hs
@@ -0,0 +1,27 @@
+<?xml version='1.0' encoding='ISO-8859-1' ?>
+<!DOCTYPE helpset PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 2.0//EN" "http://java.sun.com/products/javahelp/helpset_2_0.dtd">
+
+<!-- HelpSet auto-generated on Tue Mar 24 17:35:44 CET 2020 -->
+<helpset version="2.0">
+	<title>NTRGhidra HelpSet</title>
+	<maps>
+		<mapref location="NTRGhidra_map.xml" />
+	</maps>
+	<view mergetype="javax.help.UniteAppendMerge">
+		<name>TOC</name>
+		<label>Ghidra Table of Contents</label>
+		<type>docking.help.CustomTOCView</type>
+		<data>NTRGhidra_TOC.xml</data>
+	</view>
+	<view>
+		<name>Search</name>
+		<label>Search for Keywords</label>
+		<type>docking.help.CustomSearchView</type>
+		<data engine="com.sun.java.help.search.DefaultSearchEngine">NTRGhidra_JavaHelpSearch</data>
+	</view>
+	<view>
+		<name>Favorites</name>
+		<label>Ghidra Favorites</label>
+		<type>docking.help.CustomFavoritesView</type>
+	</view>
+</helpset>
